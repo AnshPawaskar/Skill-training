f0db0f2642d9707d038cf884ba01662e95623d36
AnshPawaskar <anshpawaskar21@gmail.com>
2025-02-20 16:22:31 +0530

    c prog

---
+++ day 4/addstudent.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student_t {
+    int id;
+    char name[100];
+    int marks[3];
+    int total;
+    float avg;
+    char grade[10];
+};
+
+typedef struct Student_t Student;
+
+void readStudents(Student students[], int n);
+void calculateGrades(Student students[], int n);
+void displayStudents(Student students[], int n);
+
+int main() {
+    int n;
+    Student students[100];
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+    readStudents(students, n);
+    calculateGrades(students, n);
+    displayStudents(students, n);
+    return 0;
+}
+
+void readStudents(Student students[], int n) {
+    printf("Enter details (ID, Name, Marks in 3 subjects):\n");
+    for (int i = 0; i < n; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("ID: ");
+        scanf("%d", &students[i].id);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        printf("Marks in 3 subjects:\n");
+        for (int j = 0; j < 3; j++) {
+            printf("Subject %d: ", j + 1);
+            scanf("%d", &students[i].marks[j]);
+        }
+    }
+}
+
+void saveStudents(Student students[], int n){
+    FILE* file = fopen("students.txt", "a");
+    if(file == NULL) {
+        printf("Cannot create file.\n");
+        return;
+    }
+    for(int i = 0;i < n;i++){
+        fprintf("%d - %s - %d",&students[i].id,students[i].name,&students[i].marks[j])
+    }
+    fclose(file);
+}
+
+void calculateGrades(Student students[], int n) {
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = 0; j < 3; j++) {
+            sum += students[i].marks[j];
+        }
+        students[i].total = sum;
+        students[i].avg = (float)sum / 3;
+
+        if (students[i].avg >= 90) {
+            strcpy(students[i].grade, "A");
+        } else if (students[i].avg >= 75) {
+            strcpy(students[i].grade, "B");
+        } else {
+            strcpy(students[i].grade, "C");
+        }
+    }
+}
+
+void displayStudents(Student students[], int n) {
+    printf("Student Report Card:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Name: %s - Total: %d, Average: %.2f, Grade: %s\n",
+               students[i].name, students[i].total, students[i].avg, students[i].grade);
+    }
+}
